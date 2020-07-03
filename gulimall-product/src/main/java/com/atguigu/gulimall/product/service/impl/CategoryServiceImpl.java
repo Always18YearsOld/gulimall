2f14c26bd76f290aea55813769e49b2e5f3478c9
Always18YearsOld <lvtianbao@qq.com>
2020-07-03 14:29:41 +0800

    49集前 管理前端菜单三级商品栏，并有append和delete 下一步需要实现删除功能

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -40,8 +41,41 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         //2.1找到所有一级分类
         List<CategoryEntity> level1Menu = entities.stream().filter((categoryEntity) -> {
             return categoryEntity.getParentCid() == 0;
-        }).collect(Collectors.toList());
+        })
+                //找出每一个菜单的子分类保存进去
+                .map((menu) -> {
+                    menu.setChildren(getChildren(menu, entities));
+                    return menu;
+                })
+                //对每一个元素进行排序
+                .sorted((menu1, menu2) -> {
+                    return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
+                })
+                //将所有数据收集到list中
+                .collect(Collectors.toList());
         return level1Menu;
     }
 
+    /**
+     * 获取当前菜单的子菜单
+     *
+     * @return
+     * @author: ltb
+     * @date 2020/7/3
+     * @params
+     */
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> {
+            return Objects.equals(categoryEntity.getParentCid(), root.getCatId());
+        }).map(categoryEntity -> {
+            //1、找到子菜单，递归找法
+            categoryEntity.setChildren(getChildren(categoryEntity, all));
+            return categoryEntity;
+        }).sorted((menu1, menu2) -> {
+            //2、菜单的排序
+            return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
+        }).collect(Collectors.toList());
+        return children;
+    }
 }
